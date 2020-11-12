ee3aeb157becc4006ed9c68f284b483270afcc8d
Nroulston <nicolas.roulston@gmail.com>
2020-11-12 16:15:14 -0700

    add an index to user email on database to prevent errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum:50}
   validates :email, presence: true, 
             length: {maximum:255}, format: { with: VALID_EMAIL_REGEX },
-            uniqueness: true
+            uniqueness: {case_sensitive: false}
   
 end
